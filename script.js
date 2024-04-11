e2fc53ac689ffd56857885992b89cc520e8657ea
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-11 21:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let s=fname.uppercase();
-return s;
+document.getElementById('fname').addEventListener('blur', function() {
+    // code to execute when the input field loses focus goes here
+});
+document.getElementById('fname').addEventListener('blur', function() {
+    let s = document.getElementById('fname').value;
+    s = s.toUpperCase();
+    document.getElementById('fname').value = s;
+});
